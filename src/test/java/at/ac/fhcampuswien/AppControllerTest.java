46dc8df86c64c9fde8a75e4c1f85e660fc2522a4
AlimYilmaz <abdulalim.yilmaz@stud.fh-campuswien.ac.at>
2022-03-30 17:28:14 +0200

    30.3. third and fourth Test

--- src/test/java/at/ac/fhcampuswien/AppControllerTest.java
+++ src/test/java/at/ac/fhcampuswien/AppControllerTest.java
@@ -29,4 +29,24 @@ public class AppControllerTest {
     @DisplayName("test if articleCount returns zero when null")
     public void testIfArticleCountIsZeroWhenArticlesNull()
     {Assertions.assertTrue(ctrl.getArticleCount() = 0);}
+
+    @Test
+    @DisplayName("test if getTopHeadLinesAustria is empty  when null")
+    public void  testIfGetTopHeadLinesAustriaIsEmptyWhenNull(){
+        Assertions.assertTrue(ctrl.getTopHeadlinesAustria().isEmpty());
+    }
+
+    @Test
+    @DisplayName("test if the searched element is in the list")
+    public void testIfTheSearchedElementIsInTheList(){
+        List<Article> articles = new ArrayList<>();
+        articles.add(new Article("Stefan Zweig", "Die Welt von Gestern"));
+        articles.add(new Article("Stephen King","I.T."));
+        articles.add(new Article("Harper Lee", "To kill a Mockingbird"));
+        articles.add(new Article("George Orwell", "1984"));
+
+        List<Article> filteredList = ctrl.filteredList("Gestern", articles);
+        Assertions.assertTrue(filteredList.size() != 0);
+    }
+
 }
