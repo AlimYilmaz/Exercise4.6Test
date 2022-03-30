e04e55a6182b04b9a31d2b3ae89b4c86feac4a0c
23Tobi47 <oluwatobi.oduniyi@stud.fh-campuswien.ac.at>
2022-03-30 11:21:38 +0200

    30.3.2022  Oluwatobi Oduniyi, Ishak Sijamhodzic, Abdulalim Yilmaz
     AppController Class

--- src/main/java/at/ac/fhcampuswien/AppController.java
+++ src/main/java/at/ac/fhcampuswien/AppController.java
@@ -3,26 +3,31 @@ package at.ac.fhcampuswien;
 import java.util.ArrayList;
 import java.util.List;
 
+// with tips from our colleagues
 public class AppController {
 
-/*
     private List<Article> articles;
 
 
     public AppController() {
-}
+    }
 
-public void setArticles(List<Article> articles) {
+    public void setArticles(List<Article> articles) {
 
-    List<Article> articles = new ArrayList<>();
-}
+    articles = new ArrayList<>();
+    }
 
 
-private static List<Article> generateMockList() {
+    private static List<Article> generateMockList() {
 
+    //https://stackoverflow.com/questions/33060592/getters-and-setters-for-arraylists-in-java (30.03.2022 11:16)
     List<Article> articles = new ArrayList<>();
     articles.add(new Article("Stefan Zweig", "Die Welt von Gestern"));
+    articles.add(new Article("Stephen King","I.T."));
+    articles.add(new Article("Harper Lee", "To kill a Mockingbird"));
+    articles.add(new Article("George Orwell", "1984"));
 
-}
- */
+    return articles;
+    }
+
 }
