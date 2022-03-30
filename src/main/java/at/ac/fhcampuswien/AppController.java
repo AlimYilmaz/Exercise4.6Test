0d754e59d7260c7e592d8bb903bc539b790a56d8
IshakYueksel <ishak.yueksel@stud.fh-campuswien.ac.at>
2022-03-30 10:43:15 +0200

    30.03
    Made Article Class together via Zoom Session
    worked on AppController but still not finished

--- src/main/java/at/ac/fhcampuswien/AppController.java
+++ src/main/java/at/ac/fhcampuswien/AppController.java
@@ -1,4 +1,28 @@
 package at.ac.fhcampuswien;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AppController {
+
+/*
+    private List<Article> articles;
+
+
+    public AppController() {
+}
+
+public void setArticles(List<Article> articles) {
+
+    List<Article> articles = new ArrayList<>();
+}
+
+
+private static List<Article> generateMockList() {
+
+    List<Article> articles = new ArrayList<>();
+    articles.add(new Article("Stefan Zweig", "Die Welt von Gestern"));
+
+}
+ */
 }
--- src/main/java/at/ac/fhcampuswien/Article.java
+++ src/main/java/at/ac/fhcampuswien/Article.java
@@ -1,4 +1,27 @@
 package at.ac.fhcampuswien;
 
 public class Article {
+
+    private String author;
+    private String title;
+
+
+    public Article(String author, String title){
+
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    @Override
+    public String toString() {
+        return "Title: " + title + "," +
+                "Author: " + author;
+    }
 }
